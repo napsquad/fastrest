cb59ca62a2a4f26044fa7bddb1cee1a0ba4070f9
napsquad <dtroutman2@live.esu.edu>
2018-07-26 15:02:35 -0400

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     @include('partials.BsCss')
-    <title>whereTo</title>
+    <title>No Mood Food</title>
 </head>
 <body class="background">
 
